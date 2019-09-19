6be64d67f20ea6d00c0ae4538c83936bffda9f1e
Krishna <ayakrishna@deloitte.com>
2019-09-19 13:45:54 +0530

    second commit

--- force-app/main/default/lwc/miscGetUserId/miscGetUserId.js
+++ force-app/main/default/lwc/miscGetUserId/miscGetUserId.js
@@ -3,6 +3,6 @@ import Id from '@salesforce/user/Id';
 
 export default class MiscGetUserId extends LightningElement {
     // Reassigning Id to a component property to provide access in the template.
-    // Since the user id doesn't change within a session, no need to @track it.
+    // Since the user id doesn't change within a session, no need to @track it
     userId = Id;
 }
